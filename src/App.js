8487278d8ff39b5120ca9cbf90c6d738335b05e1
SrKain <134458062+SrKain@users.noreply.github.com>
2024-01-27 11:59:22 -0300

    2.0
    Adicionando funcionalidades extras, e aba criar time!

--- src/App.js
+++ src/App.js
@@ -5,70 +5,116 @@ import Formulario from './componentes/forms/Forms';
 import Times from './componentes/Times/Times';
 import Footer from './componentes/Footer/Footer';
 import Separador from './componentes/Separador/Separador';
+import { v4 as uuidv4 } from 'uuid';
 
 
 function App() {
 
-  const times = [
+  const [times, setTimes] = useState([
     {
       nome: 'Programação',
-      corPrimaria: '#D9F7E9',
-      corSecundaria: '#57C278',
+      cor: '#57C278',
+      id: uuidv4(),
     },
     {
       nome: 'Front-End',
-      corPrimaria: '#E8F8FF',
-      corSecundaria: '#82CFFA',
+      cor: '#82CFFA',
+      id: uuidv4(),
     },
     {
       nome: 'Data Science',
-      corPrimaria: '#F0F8E2',
-      corSecundaria: '#A6D157',
+      cor: '#A6D157',
+      id: uuidv4(),
     },
     {
       nome: 'Devops',
-      corPrimaria: '#FDE7E8',
-      corSecundaria: '#E06B69',
+      cor: '#E06B69',
+      id: uuidv4(),
     },
     {
       nome: 'UX e Design',
-      corPrimaria: '#FAE9F5',
-      corSecundaria: '#DB6EBF',
+      cor: '#DB6EBF',
+      id: uuidv4(),
     },
     {
       nome: 'Mobile',
-      corPrimaria: '#FFF5D9',
-      corSecundaria: '#FFBA05',
+      cor: '#FFBA05',
+      id: uuidv4(),
     },
     {
       nome: 'Inovação e Gestão',
-      corPrimaria: '#FFEEDF',
-      corSecundaria: '#FF8A29',
+      cor: '#FF8A29',
+      id: uuidv4(),
     },
-  ]
+  ])
 
-  const [colabs, setColab] = useState([])
+  const [colabs, setColab] = useState([{
+    nome:'Kauan Iasin',
+    cargo:'Front-end Developer, especialista em HTML, CSS e js, com foco em React.js e TailwindCSS',
+    imagem: 'https://github.com/srkain.png',
+    time: times[0].nome,
+    favorito: true,
+    id: uuidv4(),
+  }])
 
   const newColab = (colab) => {
     console.log(colabs);
     setColab([...colabs, colab])
   }
 
+  const newTeam =(team) => {
+    console.log(team);
+    setTimes([...times, team])
+  }
+
+  function deleteColab (id) {
+    setColab(colabs.filter(colab => colab.id !== id))
+  }
+
+  function mudaCorDoTime (cor, id) {
+      setTimes(times.map(time => {
+        if(time.id === id) {
+          time.cor = cor;
+        }
+        return time;
+      }))
+  }
+
+  function favColab (id) {
+    console.log(id);
+    setColab(colabs.map(colaborador => {
+      if(colaborador.id === id) {
+        colaborador.favorito = !colaborador.favorito;
+        console.log(colaborador.favorito)
+      }
+      return colaborador;
+    }
+    )
+    )
+  } 
+  
   return (
     <div className="App">
       <Banner/>
-
-      <Formulario options={times.map((time => time.nome))} addColab={colab => newColab(colab)}/>
+      <Formulario 
+        options={times.map((time => time.nome))} 
+        addColab={colab => newColab(colab)}
+        addTeam={team => newTeam(team)}
+      />
 
       <Separador>Minha Organização:</Separador>
 
-      {times.map(time => <Times 
-        key={time.nome} 
-        nome={time.nome} 
-        corPrimaria={time.corPrimaria} 
-        corSecundaria={time.corSecundaria}
+      {times.map(time => <Times
+        mudaCor={mudaCorDoTime}
+        key={time.nome}
+        id={time.id}
+        nome={time.nome}
+        cor={time.cor}
         colabs={colabs.filter((colab => colab.time === time.nome))}
-        />)}
+        aoDeletar={id => {deleteColab(id)}}
+        favoritar={id => {favColab(id)}}
+        />)
+      }
 
         <Footer/>
 
--- src/componentes/forms/Forms.js
+++ src/componentes/forms/Forms.js
@@ -1,14 +1,19 @@
 import { useState } from "react";
 import Button from "../Button/Button";
-import CampoTexto from "../CampoTexto/CampoTexto";
+import Campo from "../Campo/Campo";
 import ListaSuspensa from "../ListaSuspensa/Lista";
 import "./Forms.css";
+import { v4 as uuidv4 } from 'uuid';
 
 const Formulario = (props) => {
   const [nome, setNome] = useState('');
   const [cargo, setCargo] = useState('');
   const [imagem, setImagem] = useState('');
   const [time, setTime] = useState('');
+  const [favorito , setFavorito] = useState(false);
+  const [nomeTime, setNomeTime] = useState('');
+  const [corTime, setCorTime] = useState('#FFFFFF');
+  const id = uuidv4();
 
 
   const aoSalvar = (evento) => {
@@ -17,12 +22,15 @@ const Formulario = (props) => {
       nome,
       cargo,
       imagem,
-      time
+      time,
+      id,
+      favorito,
     })
     setNome('');
     setCargo('');
     setImagem('');
     setTime('');
+    setFavorito(false);
   }
 
   return (
@@ -30,7 +38,7 @@ const Formulario = (props) => {
       <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o seu card de colaborador</h2>
 
-        <CampoTexto 
+        <Campo 
         obrigatório={true} 
         label="Nome" 
         placeholder="Digite seu nome" 
@@ -38,7 +46,7 @@ const Formulario = (props) => {
         setValor={valor => setNome(valor)}
         />
 
-        <CampoTexto 
+        <Campo 
         obrigatório={true} 
         label="Cargo" 
         placeholder="Digite seu cargo"
@@ -46,7 +54,7 @@ const Formulario = (props) => {
         setValor={valor => setCargo(valor)}
         />
 
-        <CampoTexto 
+        <Campo 
         label="Imagem" 
         placeholder="Informe o endereço da sua imagem"
         valor={imagem}
@@ -62,6 +70,38 @@ const Formulario = (props) => {
 
         <Button>
           Criar Card
+        </Button>
+      </form>
+      <form onSubmit={(evento) => {
+        evento.preventDefault();
+        props.addTeam({
+          nome: nomeTime,
+          cor: corTime,
+          id: uuidv4(),
+        })
+        setNomeTime('');
+        setCorTime('#ffffff');
+      }}>
+        <h2>Preencha os dados para criar o seu novo time</h2>
+
+        <Campo 
+        obrigatório={true} 
+        label="Nome" 
+        placeholder="Digite o nome do seu time" 
+        valor={nomeTime}
+        setValor={valor => setNomeTime(valor)}
+        />
+
+        <Campo 
+        obrigatório={true} 
+        label="Cor" 
+        type='color'
+        placeholder="Escolha a cor do seu time"
+        valor={corTime}
+        setValor={valor => setCorTime(valor)}
+        />
+        <Button>
+          Criar um novo time
         </Button>
       </form>
     </section>
